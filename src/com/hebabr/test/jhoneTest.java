f39780bf836ae2163240ce64b640a94759236bd0
jhone <jhonexiaomei@gmail.com>
2018-01-30 16:12:02 +0800

    更新工作流到5.22

---
+++ src/com/hebabr/test/jhoneTest.java
@@ -0,0 +1,11 @@
+package com.hebabr.test;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class) // 整合 
+@ContextConfiguration(locations="classpath:conf/spring.xml") 
+public class jhoneTest {
+	
+}
