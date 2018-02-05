077cce71a670b494eb0541cb2ff807ccfeed34fa
zhangzhiwei <jhonexiaomei@gmail.com>
2018-02-05 13:17:15 +0800

    now

--- src/com/hebabr/test/jhoneTest.java
+++ src/com/hebabr/test/jhoneTest.java
@@ -4,40 +4,35 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.zip.ZipInputStream;
-
-import org.activiti.engine.ProcessEngines;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
+import org.activiti.engine.impl.pvm.process.ActivityImpl;
 import org.activiti.engine.repository.Deployment;
-import org.activiti.engine.repository.ProcessDefinition;
-import org.activiti.engine.runtime.ProcessInstance;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.hebabr.base.dao.ActReDeploymentMapper;
-import com.hebabr.model.ActReDeployment;
+import com.hebabr.base.util.ProcessUtil;
 
-@RunWith(SpringJUnit4ClassRunner.class) // 整合 
-@ContextConfiguration(locations="classpath:conf/spring.xml") 
+@RunWith(SpringJUnit4ClassRunner.class) // 整合
+@ContextConfiguration(locations = "classpath:conf/spring.xml")
 public class jhoneTest {
-	
+
 	@Autowired
 	private RepositoryService repositoryService;
 	@Autowired
 	private RuntimeService runtimeService;
-	@Autowired
-	private ActReDeploymentMapper actReDeploymentMapper;
-	
-	
+
 	@Test
 	public void deploy() {
-		
-		String filePath = "D:\\jhoneWorkspace\\htsp.zip";
-		File file=new File(filePath);
+
+		String filePath = "D:\\Desktop.zip";
+		File file = new File(filePath);
 		InputStream in;
 		try {
 			in = new FileInputStream(file);
@@ -47,21 +42,23 @@ public class jhoneTest {
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	@Test
 	public void startProcessInstance() {
-		
-		ProcessEngines.getDefaultProcessEngine().getRuntimeService()// 获取正在执行的Service
-				.startProcessInstanceById("contract_jiancezhongxin:1:4");// 按照流程定义的Id
+
+		Map<String, Object> variables = new HashMap<String, Object>();
+		variables.put("enter", "jhone");
+		runtimeService.startProcessInstanceById("contract_jiancezhongxin:1:4", variables);// 按照流程定义的Id
 	}
-	
+
 	@Test
-	public void getProDefine() {
-		ProcessDefinition pde = repositoryService.getProcessDefinition("contract_jiancezhongxin:1:4");
-		actReDeploymentMapper.selectByPrimaryKey("123");
+	public void getActivity() {
+
+		ActivityImpl activityImpl = ProcessUtil.getActivity(repositoryService, "contract_jiancezhongxin:1:4",
+				"usertask1");
+		System.out.println(activityImpl);
 	}
-	
-	
+
 }
